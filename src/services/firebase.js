df9f77eb756d32ee67a68da8cd6534796c987310
LUIS FABIANO MARTINS <martinsxis777@gmail.com>
2026-02-26 17:25:54 -0300

    configurado firebase do cliente e ativada gateway

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 // Para o front-end em React, precisamos da configuração web (Web API Key).
 // Substitua o 'sua_api_key_aqui' pela Web API Key real do seu projeto Firebase.
 const firebaseConfig = {
-    apiKey: "AIzaSyDxByd1WfVyGSVlwQbnvd05lTJ2AxixJ5w",
-    authDomain: "cardapio-f5b6e.firebaseapp.com",
-    projectId: "cardapio-f5b6e",
-    storageBucket: "cardapio-f5b6e.firebasestorage.app",
-    messagingSenderId: "889279350354",
-    appId: "1:889279350354:web:e2b8eb0f38b3a55e15818b"
+    apiKey: "AIzaSyCt4blW0Vf-REz8Y587SKEd0Otz_PGUvGw",
+  authDomain: "trampo-tela.firebaseapp.com",
+  projectId: "trampo-tela",
+  storageBucket: "trampo-tela.firebasestorage.app",
+  messagingSenderId: "891787716905",
+  appId: "1:891787716905:web:f2be2badf1ca2c50daef38"
 };
 
 // Initialize Firebase
